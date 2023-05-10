406d26449d0d5a38974adf936eb49e7fec35b2fd
Patrick McLean <pmclean26@amherst.edu>
2023-05-09 23:42:03 -0400

    improved spacing of appends in world

--- src/World.java
+++ src/World.java
@@ -28,6 +28,7 @@ public class World {
         this.allCoins = new DataStructure<Coin>();
         this.special = new DataStructure<Obstacle>();
 
+        allObstacles.append(new InGameLogo(50, 100));
 
         allObstacles.append(new Brick(650, 425));
         allObstacles.append(new Brick(750, 300));
@@ -42,26 +43,25 @@ public class World {
         allObstacles.append(new Brick(1300, 180));
         allCoins.append(new Coin(1300, 100));
         allObstacles.append(new Brick(1340, 180));
-        allObstacles.append(new InGameLogo(50, 100));
+
         allObstacles.append(new Pipe(756, Main.HEIGHT - 100 - 100, 70, 100));
+
         allObstacles.append(new Brick(1250, 500));
         allObstacles.append(new Block(1100, 500));
         allObstacles.append(new Brick(950, 500));
         allObstacles.append(new Block(1350, 628));
         allObstacles.append(new StairBlock(1400, 588));
+
         allObstacles.append(new Pipe(1600, Main.HEIGHT - 100 - 130, 70, 130));
+
         for (int i = 4; i > 0; i--){
             for (int j = 0; j < i; j++){
                 allObstacles.append(new StairBlock(1800 + (i * 40), 628 - (j * 40)));
             }
         }
+
         allObstacles.append(new Pipe(2250, Main.HEIGHT - 100 - 140,70, 140));
-        allCoins.append(new Coin(2270, 350));
-        allCoins.append(new Coin(1100, 400));
-        allCoins.append(new Coin(1622, 360));
-        allNPCs.append(new NPC(2100, 300, this));
-        allNPCs.append(new NPC(2270, 300, this));
-        allNPCs.append(new NPC(1200, 500, this));
+
         for (int i = 0; i < 9; i++){
             for (int j = 0; j < i; j++){
                 allObstacles.append(new StairBlock(2400 + (i * 40), 628 - (j * 40)));
@@ -70,6 +70,14 @@ public class World {
         for (int i = 0; i < 8; i++){
             allObstacles.append(new StairBlock(2400 + (9 * 40), 628 - (i * 40)));
         }
+
+        allCoins.append(new Coin(2270, 350));
+        allCoins.append(new Coin(1100, 400));
+        allCoins.append(new Coin(1622, 360));
+        allNPCs.append(new NPC(2100, 300, this));
+        allNPCs.append(new NPC(2270, 300, this));
+        allNPCs.append(new NPC(1200, 500, this));
+
         special.append(new FlagStaff(3070, Main.HEIGHT - 100 - 320, 320));
     }
     public void draw(Graphics g) {
